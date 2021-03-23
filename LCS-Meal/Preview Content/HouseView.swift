266fef1c3f76e66784442a8b80deb756e6046a8f
yining wang <ywang@lcs.on.ca>
2021-03-23 15:09:05 -0400

    Add the nevigation link of the schedule view and the file

--- LCS-Meal/Preview Content/HouseView.swift
+++ LCS-Meal/Preview Content/HouseView.swift
@@ -8,10 +8,24 @@
 import SwiftUI
 
 struct HouseView: View {
+    @State private var apiData = ApiData()
+    private var houses:[String] = ["Ryder", "Wads", "Lower", "Ondaatje", "Matthews", "Uplands", "Memorial", "Moodie", "Cooper", "Grove", "Upper", "Rashleigh"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            List{
+                ForEach(0..<houses.count){ num in
+                    
+                    NavigationLink(destination: ScheduleView(details: apiData.filter(house: houses[num]), house: houses[num])){
+                        Text("\(houses[num])")
+                    }
+                }
+            }
+            .navigationBarTitle("Houses")
+        }
     }
 }
+
 
 struct HouseView_Previews: PreviewProvider {
     static var previews: some View {
