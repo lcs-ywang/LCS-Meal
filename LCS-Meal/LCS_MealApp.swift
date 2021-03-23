3dfeaa83bf7ecf8b71b6dc0197c6cf3fa1a59734
yining wang <ywang@lcs.on.ca>
2021-03-23 14:11:18 -0400

    Initial Commit

---
+++ LCS-Meal/LCS_MealApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCS_MealApp.swift
+//  LCS-Meal
+//
+//  Created by Yining Wang on 2021-03-23.
+//
+
+import SwiftUI
+
+@main
+struct LCS_MealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
