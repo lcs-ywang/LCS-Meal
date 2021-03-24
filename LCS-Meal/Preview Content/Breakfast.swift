988bcf84ca700e096cba0ed7a42a0195dd301b09
yining wang <ywang@lcs.on.ca>
2021-03-24 17:02:44 -0400

    Add the date Formatter for the breakfast preview

--- LCS-Meal/Preview Content/Breakfast.swift
+++ LCS-Meal/Preview Content/Breakfast.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct Breakfast: View {
+    @State var details:[Menu] = []
+    private var todayDate : String{
+        let today = Date()
+        let formatter1 = DateFormatter()
+        formatter1.dateStyle = .short
+
+        return (formatter1.string(from: today))
+    }
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
