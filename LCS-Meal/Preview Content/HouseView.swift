df792a31b52cdb875954b5761dc6978d3bc3d235
yining wang <ywang@lcs.on.ca>
2021-03-24 17:54:20 -0400

    Added Navigation View

--- LCS-Meal/Preview Content/HouseView.swift
+++ LCS-Meal/Preview Content/HouseView.swift
@@ -12,16 +12,18 @@ struct HouseView: View {
     private var houses:[String] = ["Ryder", "Wads", "Lower", "Ondaatje", "Matthews", "Uplands", "Memorial", "Moodie", "Cooper", "Grove", "Upper", "Rashleigh"]
     
     var body: some View {
-        VStack{
-            List{
-                ForEach(0..<houses.count){ num in
-                    
-                    NavigationLink(destination: ScheduleView(details: apiData.filter(house: houses[num]), house: houses[num])){
-                        Text("\(houses[num])")
+        NavigationView{
+            VStack{
+                List{
+                    ForEach(0..<houses.count){ num in
+                        
+                        NavigationLink(destination: ScheduleView(details: apiData.filter(house: houses[num]), house: houses[num])){
+                            Text("\(houses[num])")
+                        }
                     }
                 }
+                .navigationBarTitle("Houses")
             }
-            .navigationBarTitle("Houses")
         }
     }
 }
